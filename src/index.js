4de333600de26ee017153f0f1145deb9b84c7f4c
孙好省 <wb-shs456320@alibaba-inc.com>
2019-11-15 11:55:43 +0800

    fix: document bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.css';
 import Ppt from './tools/ppt/initPpt';
 new Ppt( '#pptCanvas', '#sidebar' );
-document.querySelector('button').onclick = function(){
-    document.querySelector('.asd').classList.add('dsa');
-}
+// document.querySelector('button').onclick = function(){
+//     document.querySelector('.asd').classList.add('dsa');
+// }
 export default Ppt;
