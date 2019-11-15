36af03969e77f97646ddd557b11cf2e7561fa032
孙好省 <wb-shs456320@alibaba-inc.com>
2019-11-15 11:57:19 +0800

    fix: document bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import Ppt from './tools/ppt/initPpt';
-new Ppt( '#pptCanvas', '#sidebar' );
+// new Ppt( '#pptCanvas', '#sidebar' );
 // document.querySelector('button').onclick = function(){
 //     document.querySelector('.asd').classList.add('dsa');
 // }
